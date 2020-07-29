2dc61c7a9d0fc3c82ca7f4ff377213f2fb2c7d5d
maiconmoretto <maiconmorettos@gmail.com>
2020-07-29 20:43:37 -0300

    created title and value for last releases

--- src/components/ListExpenses.tsx
+++ src/components/ListExpenses.tsx
@@ -5,12 +5,15 @@ import { IonList, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox,
 export const ListExpenses: React.FC = () => (
   <IonContent>
     <IonItem>
-      <div id="ultimos-lancamentos">Ultimos lançamentos</div>
+      <div id="last-releases">Ultimos lançamentos</div>
     </IonItem>
     {/*-- List of Text Items --*/}
     <IonList>
          <IonItem>
-        <IonLabel>Super Mario World</IonLabel>
+        <IonLabel class="title">Super Mario World</IonLabel>
+        <IonLabel class="value">300</IonLabel>
+
+
       </IonItem>
     </IonList>
 
