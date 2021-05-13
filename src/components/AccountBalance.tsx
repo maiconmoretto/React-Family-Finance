8b2a9518dc2c55dcd7455f428a5918a7f63f134d
maiconmoretto <maiconmorettos@gmail.com>
2021-05-13 18:25:31 -0300

    Adjust in position of account balance

--- src/components/AccountBalance.tsx
+++ src/components/AccountBalance.tsx
@@ -5,9 +5,7 @@ import { IonList, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox,
 export const AccountBalance: React.FC = () => (
   <IonContent id="content-account-balance">
     <IonItem lines="none">     
-      <div id="title">Saldo na conta</div>
-    </IonItem>
-    <IonItem lines="none">     
+      <div id="account-balance">Saldo na conta</div>
       <div id="value-account-balance"> R$ 2000,00</div>
     </IonItem>
   </IonContent>
